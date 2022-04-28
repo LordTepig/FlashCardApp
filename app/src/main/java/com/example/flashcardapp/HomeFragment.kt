544bd2f5a75f52ba43621ad9ca2c43106f8fc926
LordTepig <tyler8555@gmail.com>
2022-04-28 12:13:54 -0400

    up to date progess 4/27/22

--- app/src/main/java/com/example/flashcardapp/HomeFragment.kt
+++ app/src/main/java/com/example/flashcardapp/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.flashcardapp.databinding.FragmentMainBinding
 
 
@@ -20,16 +21,20 @@ class HomeFragment : Fragment() {
         val rootView = binding.root
 
         binding.addFlashCard.setOnClickListener {
-
+            val action = HomeFragmentDirections.actionHomeFragmentToCreateFlashCardFragment()
+            rootView.findNavController().navigate(action)
         }
         binding.addMultipleChoiceCard.setOnClickListener {
-
+            val action = HomeFragmentDirections.actionHomeFragmentToCreateMultipleChoiceFragment()
+            rootView.findNavController().navigate(action)
         }
         binding.addTrueFalseCard.setOnClickListener {
-
+            val action = HomeFragmentDirections.actionHomeFragmentToCreateTrueFalseFragment()
+            rootView.findNavController().navigate(action)
         }
         binding.studyCurrentQuestions.setOnClickListener {
-
+            val action = HomeFragmentDirections.actionHomeFragmentToStudyCardsFragment(0)
+            rootView.findNavController().navigate(action)
         }
         return rootView
     }
