9d12b5a32d9c63155ec1511f204ec623bcf5f814
LordTepig <tyler8555@gmail.com>
2022-05-04 12:14:38 -0400

    up to date progess 5/4/22

--- app/src/main/java/com/example/flashcardapp/QuestionViewModel.kt
+++ app/src/main/java/com/example/flashcardapp/QuestionViewModel.kt
@@ -3,4 +3,8 @@ package com.example.flashcardapp
 import androidx.lifecycle.ViewModel
 
 class QuestionViewModel: ViewModel() {
+    private var _questions = mutableListOf<Question>()
+    val questions: MutableList<Question>
+        get() = _questions
+
 }
--- app/src/main/java/com/example/flashcardapp/StudyCardsFragment.kt
+++ app/src/main/java/com/example/flashcardapp/StudyCardsFragment.kt
@@ -6,12 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
 import com.example.flashcardapp.databinding.FragmentStudyCardsBinding
 
 class StudyCardsFragment : Fragment() {
     private val viewModel: QuestionViewModel by viewModels()
     private var _binding: FragmentStudyCardsBinding? = null
     private val binding get() = _binding!!
+
+    val questions  = mutableListOf<Question>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,11 +24,16 @@ class StudyCardsFragment : Fragment() {
         val rootView = binding.root
 
         val args = StudyCardsFragmentArgs.fromBundle(requireArguments())
-        val questions = listOf(Question(args.questionArg, args.answerArg)) //default question
+        val ques = Question(args.questionArg, args.answerArg)
+        questions.add(ques) //default question
 
         val adapter = QuestionAdapter(questions)
         binding.recyclerView.adapter = adapter
 
+        binding.returnHomeButton.setOnClickListener {
+            val action = StudyCardsFragmentDirections.actionStudyCardsFragmentToHomeFragment()
+            rootView.findNavController().navigate(action)
+        }
         return rootView
     }
 
