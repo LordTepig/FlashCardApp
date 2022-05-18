ce43fb8376000b8dec2588f2d00fd83b4e69d7db
LordTepig <tyler8555@gmail.com>
2022-05-18 12:12:13 -0400

    up to date progess 5/18/22

--- app/src/main/java/com/example/flashcardapp/CreateFlashCardFragment.kt
+++ app/src/main/java/com/example/flashcardapp/CreateFlashCardFragment.kt
@@ -1,10 +1,12 @@
 package com.example.flashcardapp
 
+import android.app.ProgressDialog.show
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.findNavController
 import com.example.flashcardapp.databinding.FragmentCreateFlashCardBinding
 import com.example.flashcardapp.databinding.FragmentMainBinding
@@ -20,14 +22,30 @@ class CreateFlashCardFragment : Fragment() {
     ): View? {
         _binding = FragmentCreateFlashCardBinding.inflate(inflater, container, false)
         val rootView = binding.root
-
+//        lateinit var question: String
         binding.addFlashCardButton.setOnClickListener {
+//            while(binding.flashCardQuestionEditText.text.toString() != ""){
+//                Toast.makeText(context, R.string.no_question, Toast.LENGTH_LONG).show()
+//                //display a message that says the user needs to input text for the question
+//                }
+//            while(binding.answerEditText.text.toString() != ""){
+//                Toast.makeText(context, R.string.no_answer, Toast.LENGTH_LONG).show()
+//            }
             val question = binding.flashCardQuestionEditText.text.toString()
             val answer = binding.answerEditText.text.toString()
-            val action = CreateFlashCardFragmentDirections.actionCreateFlashCardFragmentToStudyCardsFragment(question, answer)
-            rootView.findNavController().navigate(action)
+            if ((question != "")&& (answer != "")){
+                val action = CreateFlashCardFragmentDirections.actionCreateFlashCardFragmentToStudyCardsFragment(question, answer)
+                rootView.findNavController().navigate(action)
+            }
+            else{
+                if(binding.flashCardQuestionEditText.text.toString() == "") {
+                    Toast.makeText(context, R.string.no_question, Toast.LENGTH_LONG).show()
+                }
+                else if(binding.answerEditText.text.toString() == ""){
+                    Toast.makeText(context, R.string.no_answer, Toast.LENGTH_LONG).show()
+                }
+            }
         }
-
         return rootView
     }
 
